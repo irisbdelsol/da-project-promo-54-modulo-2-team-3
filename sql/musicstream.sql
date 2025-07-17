db3fc27926cba91bc6f6be0cbfaff479614e84fe
Luciana Yelicich <yelicichluciana@gmail.com>
2025-07-17 14:36:28 +0200

    sql con enunciados de consultas

--- sql/musicstream.sql
+++ sql/musicstream.sql
@@ -1,1 +1,17 @@
 USE musicstream;
+
+-- ¿Cuál es el artista con más albums?
+
+-- ¿Qué género es el mejor valorado?
+
+-- ¿En qué año se lanzaron más álbumes?
+
+-- ¿Cuál es la canción mejor valorada?
+
+-- ¿Cuál es el artista con más valoración?
+
+-- ¿Cuál es el album más valorado de los años pares de mi selección?
+
+-- ¿Qué país tiene má artistas? (ordenar por popularidad)
+
+-- ¿Qué artista estuvo más tiempo y cuántos albums tiene?
